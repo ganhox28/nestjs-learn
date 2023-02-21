f9732184811dd4a0fcba93826f48e1a8f5555873
ganhox28 <nguyenquocanhquan69@gmail.com>
2023-02-21 16:52:37 +0700

    check

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+
+@Controller({})
+
+export class AuthController {
+    constructor(private authService: AuthService){
+        
+    }
+
+    @Post("register")
+    register() {
+        
+    }
+    @Post("login")
+    login() {
+        
+    }
+}
